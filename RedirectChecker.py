d89d6a304d860f4078891bad969e3b5fc60df7dc
Irvin H <irvinhomem@gmail.com>
2016-05-31 16:29:38 +0200

    Creating/writing new CSV file with domains and HTTP / HTTPS designation

--- RedirectChecker.py
+++ RedirectChecker.py
@@ -1,12 +1,24 @@
 import requests
-import string
 import csv
+from tempfile import NamedTemporaryFile
+import shutil
+import logging
 
 class RedirectChecker(object):
 
     def __init__(self):
-        self.CsvFilePath = 'top-50-Cleaned.csv'
+        # Configure Logging
+        #logging.basicConfig(level=logging.INFO)
+        #logging.basicConfig(level=logging.WARNING)
+        self.logger = logging.getLogger(__name__)
+        # self.logger.setLevel(logging.INFO)
+        #self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.WARNING)
+
+        self.CsvFilePath = 'top-5.csv' #'top-50-Cleaned.csv'
         self.all_domains = []
+        self.csvOutputPath = 'Labeled-http-50.csv'
+        self.all_row_data =[]
 
     def read_csv_file(self):
         csv_filepath = self.CsvFilePath
@@ -22,6 +34,7 @@ class RedirectChecker(object):
         http_req_headers = {'user-agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0'}
 
         for count, domain in enumerate(self.all_domains):
+            single_row = []
             url = "http://"+ domain
             try:
                 response = requests.get(url, timeout=40, headers=http_req_headers)  #headers=http_req_headers  # allow_redirects=True
@@ -40,13 +53,44 @@ class RedirectChecker(object):
                 #print("%i : [%i] %s \t: %s :\t Redirects: %i" % (count, status_code, domain, protocol, no_of_redirs))
                 print("%i : [%i] %s \t: %s :\t Location-Proto: [%s] \t: Redirects: %i"
                       % (count, status_code, domain, protocol, location_proto, no_of_redirs))
+                # single_row['Domain'] = domain
+                # single_row['Proto'] = protocol
+
+                single_row.append(domain)
+                single_row.append(protocol)
+                single_row.append(location_proto)
             except:
                 print("%i : %s \t: Timeout" % (count, domain))
+                single_row.append(domain)
+                single_row.append("Timeout")
+                single_row.append("Timeout")
+
+            self.all_row_data.append(single_row)
+            self.logger.debug("SINGLE-ROW: Domain: [%s] | Proto: [%s] | Loc-Proto: [%s]" % (single_row[0], single_row[1], single_row[2]))
+            self.logger.debug("Number of rows: %i" % len(self.all_row_data))
+
+    def write_csv_file(self):
+        csv_out = self.csvOutputPath
+        tempfile = NamedTemporaryFile(delete=False)
+        self.logger.debug("Domain: [%s] | Proto: [%s] | Loc-Proto: [%s]" % (self.all_row_data[0][0], self.all_row_data[0][1], self.all_row_data[0][2]))
+        self.logger.debug("Domain: [%s] | Proto: [%s] | Loc-Proto: [%s]" % (self.all_row_data[1][0], self.all_row_data[1][1], self.all_row_data[1][2]))
+        with open(csv_out, mode = 'w', newline='') as tempfile:
+            writer = csv.writer(tempfile, delimiter=',')
+
+            for row in self.all_row_data:
+                #writer.writerow([row.item])
+                self.logger.debug("Domain: [%s] | Proto: [%s] | Loc-Proto: [%s]" % (row[0], row[1], row[2]))
+                writer.writerow([row[0],row[1],row[2]])
+                #if row[1] == 'https' or row[2] == 'https':
+
+        shutil.move(tempfile.name, self.csvOutputPath)
+
 
 redirChk = RedirectChecker()
 
 redirChk.read_csv_file()
 redirChk.check_https()
+redirChk.write_csv_file()
 
 
 
