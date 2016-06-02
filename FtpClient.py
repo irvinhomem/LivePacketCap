04c5c8bafa4c81bf81c42d10f2d8ab9d03eee13a
Irvin H <irvinhomem@gmail.com>
2016-06-02 20:46:25 +0200

    Backing up ...

--- FtpClient.py
+++ FtpClient.py
@@ -1,3 +1,4 @@
+#from ftplib import FTP
 import ftplib
 import csv
 import logging
@@ -16,10 +17,13 @@ class FtpClient(object):
 
         self.configs_path = './csv_files/ftp_creds.csv'
         self.configs_dict = {}
-        # self.server_ip = ''
-        # self.server_port = ''
-        # self.user = ''
-        # self.pwd = ''
+        self.server_ip = ''
+        self.server_port = ''
+        self.user = ''
+        self.pwd = ''
+        self.home_dir = '/'
+
+        self.client = None
         self.logger.info("Created FTP Client ...")
 
     def read_configs(self):
@@ -31,12 +35,16 @@ class FtpClient(object):
             for row in domain_name_rdr:
                 if row[0] == 'ip':
                     self.configs_dict['server_ip'] = row[1]
+                    self.server_ip = row[1]
                 elif row[0] == 'port':
                     self.configs_dict['server_port'] = row[1]
+                    self.server_port = row[1]
                 elif row[0] == 'user':
                     self.configs_dict['user'] = row[1]
+                    self.user = row[1]
                 elif row[0] == 'pwd':
                     self.configs_dict['pwd'] = row[1]
+                    self.pwd = row[1]
                 else:
                     self.logger.debug("Unknown config parameter found")
 
@@ -45,10 +53,60 @@ class FtpClient(object):
         self.logger.debug("User: %s" % (self.configs_dict['user']))
         self.logger.debug("Pass: %s" % (self.configs_dict['pwd']))
 
+    def login(self):
+        # ip = self.configs_dict['server_ip']
+        # port = self.configs_dict['server_port']
+        # username = self.configs_dict['user']
+        # pwd = self.configs_dict['pwd']
+
+        self.client = ftplib.FTP(host=self.server_ip)       #FTP(host=ip, user=username, passwd=pwd)
+        self.client.login(user=self.user, passwd=self.pwd)
+        self.logger.debug("Welcome Message: %s" % self.client.getwelcome())
+        #self.client.cwd(self.home_dir)
+
+    def listCurrDir(self):
+        self.logger.debug("Working Directory: %s" % self.client.pwd())
+        self.client.dir()
+
+        # try:
+        #     #dir_objs = self.client.mlsd(path='/', facts=['type', 'size', 'perm'])
+        #     dir_objs = self.client.mlsd()
+        #     self.logger.debug("Directory List Type: %s" % dir_objs)
+        #
+        #     for f in dir_objs:
+        #         print(type(f[0]))
+        # except ftplib.all_errors as exception:
+        #     #self.logger.warn(exception.)
+        #     raise
+
+    def list_file_names(self):
+        #list = self.client.nlst()
+        #print(list[0])
+        for file_name in self.client.nlst():
+            print("Files and Directories: %s" % file_name)
+
+    def list_directories(self):
+        for file_name in self.client.nlst():
+            try:
+                self.client.cwd(file_name)
+                print("Directory: %s" % file_name)
+            except ftplib.error_perm as detail:
+                print("It's probably not a directory:", detail)
+
+    def ftp_log_out(self):
+        self.client.quit()
+
 
 myFTPClient = FtpClient()
 myFTPClient.read_configs()
+myFTPClient.login()
 
+
+myFTPClient.listCurrDir()
+myFTPClient.list_file_names()
+myFTPClient.list_directories()
+
+myFTPClient.ftp_log_out()
 
 
 
