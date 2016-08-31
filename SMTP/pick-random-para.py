d4777d5ef940085e22bcf2e969cb5fcb35e8ccbe
Irvin H <irvinhomem@gmail.com>
2016-08-31 20:00:25 +0200

    Backing up ... not yet working

--- SMTP/pick-random-para.py
+++ SMTP/pick-random-para.py
@@ -1,5 +1,6 @@
 import random
 import logging
+import linecache
 
 # Configure Logging
 logging.basicConfig(level=logging.INFO)
@@ -20,22 +21,29 @@ def get_paragraph_line_number_starts(txt_f):
     paragraph_starts = []
     my_file = open(txt_f, 'r')
     for count, line in enumerate(my_file):
-        logger.debug('COUNT: %i' % count)
-        logger.debug('LINE: %s' % line)
-        try:
-            line2 = my_file.readline()
-            logger.debug('**Line 1: %s' % line2)
-            line3 = my_file.readline()
-            logger.debug('**Line 2: %s' % line3)
+        # logger.debug('COUNT: %i' % count)
+        # logger.debug('**LINE 1: %s' % line)
 
+        line1 = linecache.getline(text_file, count + 1)
+        logger.debug('Line: %s' % str(line1))
+        line2 = linecache.getline(text_file, count + 2)
+        logger.debug('Line: %s' % str(line2))
+        line3 = linecache.getline(text_file, count + 3)
+        logger.debug('Line: %s' % str(line3))
 
-            # if line.endswith('\n') and my_file.readline(count+1).startswith('\n'):
-            #     if not my_file.readline(count+2).startswith('\n'):
-            #         paragraph_starts.append(count+2)
-            #         logger.debug('COUNT +2: %s' % str(count+2))
-            #         logger.debug('Para start: %s' % str(my_file.readline()))
-        except:
-            logger.debug('EOL or some other error')
+        # try:
+        #     line2 = my_file.readline()
+        #     logger.debug('**Line 2: %s' % line2)
+        #     line3 = my_file.readline()
+        #     logger.debug('**Line 3: %s' % line3)
+        #
+        #     if line.endswith('\n') and line2.startswith('\n'):
+        #         if not line3.startswith('\n'):
+        #             paragraph_starts.append(count+2)
+        #             logger.debug('COUNT +2: %s' % str(count+2))
+        #             #logger.debug('Para start: %s' % str(linecache.getline(my_file, count+2)))
+        # except:
+        #     logger.debug('EOL or some other error')
 
     logger.debug('Paragraph starts list length: %i' % len(paragraph_starts))
     logger.debug('Paragraph 1 line num: %i' % paragraph_starts[0])
